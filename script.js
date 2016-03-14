ab76a24420051bd7909d0c5980233b40a7a82fbe
Kim Gunnarsson <kim@morgondag.nu>
2016-03-14 07:53:41 +0100

    Branding!

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ window.onload = function(){
 	clickComments();
 }
 
+var setBodyClass = function(){
+	var brand = window.location.hostname.split('.')[0];
+	var body = document.querySelector('body');
+	body.classList.add(brand);
+}
+setBodyClass();
+
 // show comments only..
 var clickComments = function(){
 	var tabPanel = document.querySelector('#comment-tabpanel');
