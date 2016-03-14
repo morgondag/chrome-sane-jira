6f87126693719d8ca2f7bd595ba8a98e9a3c6f34
Kim Gunnarsson <kim@morgondag.nu>
2016-03-14 07:24:13 +0100

    comments or gtfo

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello worlds');
+
+
+window.onload = function(){
+	clickComments();
+}
+
+// show comments only..
+var clickComments = function(){
+	var tabPanel = document.querySelector('#comment-tabpanel');
+	if(!tabPanel){
+		return;
+	}
+	tabPanel.click();
+	var commentsTab = document.querySelector('.tabs2');
+	if(!commentsTab){
+		return;
+	}
+	commentsTab.style.display = "none";
+}
